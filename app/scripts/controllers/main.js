21480de96beea3666362746c189fc7711304d439
viehot <piri.viejo@gmail.com>
2014-10-15 19:20:45 -0300

    Borrar y agregar Todo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('mytodoAppApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+});
